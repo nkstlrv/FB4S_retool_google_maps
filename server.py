18eda543730c78624e58b739762cfe8160fc229b
Nikita <stlrvnk@gmail.com>
2023-08-29 13:58:45 +0300

    map.html as index page

--- server.py
+++ server.py
@@ -4,20 +4,13 @@ server = Flask(__name__)
 
 
 @server.route("/", methods=["GET"])
-def index():
-    return jsonify(
-        {"message": "Hello World"}
-    )
+def map():
+    return render_template("map.html")
 
 
 @server.route("/map_old")
 def map_old_page():
     return render_template("map_old.html")
-
-
-@server.route("/map_new")
-def map_new_page():
-    return render_template("map_new.html")
 
 
 if __name__=='__main__':
